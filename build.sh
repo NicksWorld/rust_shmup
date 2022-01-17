a7a9fb7b6401c1af06d72ec30c1abcfe474692fc
NicksWorld <nickmcdaniel00@gmail.com>
2022-01-17 17:18:10 -0500

    feat<buildscript>: Update the buildscripts to not rely on symlinks

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cargo build --target x86_64-unknown-linux-gnu 
+cargo build --target x86_64-unknown-linux-gnu
 mkdir -p ./godot/libshmup_rust
 mv -b ./target/x86_64-unknown-linux-gnu/debug/*.so ./godot/libshmup_rust/
-godot --path godot/ -d
